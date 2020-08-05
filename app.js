0c0d53cae94b8bc6865d4e1715ac554ade5269fc
Dean Hildebrand <dean.hildebrand88@gmail.com>
2020-08-05 17:13:22 -0400

    created put/patch routes for specific article

--- app.js
+++ app.js
@@ -22,44 +22,124 @@ const articleSchema = {
 
 const Article = mongoose.model("Article", articleSchema);
 
-// Restful GET route for ALL articles
-app.get("/articles", function(req, res) {
-  Article.find({}, function(err, foundArticles) {
-    if (!err) {
-      res.send(foundArticles);
-    } else {
-      res.send(err);
-    }
+// app.route allows for multiple methods to chain to the same route
+app
+  .route("/articles")
+  .get(function(req, res) {
+    Article.find({}, function(err, foundArticles) {
+      if (!err) {
+        res.send(foundArticles);
+      } else {
+        res.send(err);
+      }
+    });
+  })
+  .post(function(req, res) {
+    // create new article and save the key values.
+    const newArticle = new Article({
+      title: req.body.title,
+      content: req.body.content
+    });
+    // Check for err if none, then save article
+    newArticle.save(function(err) {
+      if (!err) {
+        res.send("Successfully added a new article");
+      } else {
+        res.send(err);
+      }
+    });
+  })
+  .delete(function(req, res) {
+    Article.deleteMany(function(err) {
+      if (!err) {
+        res.send("Successfully deleted all articles");
+      } else {
+        res.send(err);
+      }
+    });
   });
-});
 
-// Restful POST route to add a NEW article
-app.post("/articles", function(req, res) {
-  // create new article and save the key values. r
-  const newArticle = new Article({
-    title: req.body.title,
-    content: req.body.content
-  });
-  // Check for err if none, then save article
-  newArticle.save(function(err) {
-    if (!err) {
-      res.send("Successfully added a new article");
-    } else {
-      res.send(err);
-    }
-  });
-});
+// ///////////////////////Request targeting Specific article ////////////////
 
-app.delete("/articles", function(req, res) {
-  Article.deleteMany(function(err) {
-    if (!err) {
-      res.send("Successfully deleted all articles");
-    } else {
-      res.send(err);
+app
+  .route("/articles/:articleTitle")
+
+  .get(function(req, res) {
+    Article.findOne({ title: req.params.articleTitle }, function(
+      err,
+      foundArticle
+    ) {
+      if (foundArticle) {
+        res.send(foundArticle);
+      } else {
+        res.send("No articles matching that title was found");
+      }
+    })
+  })
+
+  .put(function(req, res) {
+    Article.updateOne(
+      {title: req.params.articleTitle},
+      {title: req.body.title, content: req.body.content}, {overwrite: true},
+      function(err){
+      if (!err) {
+        res.send("Successfully edited the article")
+      }
+    })
+})
+
+.patch(function(req, res){
+  Article.updateOne(
+    {title: req.params.articleTitle},
+    {$set: req.body},
+    function(err){
+      if (!err) {
+        res.send("Successfully updated the article")
+      } else {
+        res.send(err)
+      }
     }
-  });
-});
+  )
+})
 
 app.listen(3000, function() {
   console.log("Successfully started the local server");
 });
+
+// Restful GET route for ALL articles
+// app.get("/articles", function(req, res) {
+//   Article.find({}, function(err, foundArticles) {
+//     if (!err) {
+//       res.send(foundArticles);
+//     } else {
+//       res.send(err);
+//     }
+//   });
+// });
+
+// Restful POST route to add a NEW article
+// app.post("/articles", function(req, res) {
+//   // create new article and save the key values.
+//   const newArticle = new Article({
+//     title: req.body.title,
+//     content: req.body.content
+//   });
+//   // Check for err if none, then save article
+//   newArticle.save(function(err) {
+//     if (!err) {
+//       res.send("Successfully added a new article");
+//     } else {
+//       res.send(err);
+//     }
+//   });
+// });
+
+// app.delete("/articles", function(req, res) {
+//   Article.deleteMany(function(err) {
+//     if (!err) {
+//       res.send("Successfully deleted all articles");
+//     } else {
+//       res.send(err);
+//     }
+//   });
+// });
